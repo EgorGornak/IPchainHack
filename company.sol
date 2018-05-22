d74d701c459d26ece5d179d515feaa9d64e79e6c
Egor Gornak <31937056+EgorGornak@users.noreply.github.com>
2018-05-23 01:32:05 +0300

    Update company.sol

--- company.sol
+++ company.sol
@@ -1,30 +1,31 @@
 pragma solidity ^0.4.22;
 
 contract company {
-    struct certificate { 
+    struct certificate {
+        string name;
         string picture;
         mapping (address => bool) isValid;
     }
                 
-    address Company = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
-    certificate[] public Certificates; 
+    address Company = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9; // for example
+    mapping (string => certificate) Certificates;
     
-    function addCertificate(string pic) public  {
+    function addCertificate(string pic, string name) public  {
         if (msg.sender != Company) {
             return;
         }
         certificate tmp;
+        tmp.name = name;
         tmp.picture = pic;
-        Certificates.push(tmp);
+        Certificates[name] = tmp;
     }
     
-    function giveCertificate(uint certificateID, address receiver) public  {
+    function giveCertificate(string name, address receiver) public  {
         if (msg.sender != Company) {
             return;
         }
-        if (certificateID >= Certificates.length) {
-            return;
-        }
-        Certificates[certificateID].isValid[receiver] = true;
+        Certificates[name].isValid[receiver] = true;
+        address dc = receiver;
+        require(dc.call(bytes4(keccak256("recieveCert(string)")), name));
     }
 }
