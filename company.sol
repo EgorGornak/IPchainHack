d8d5c7f313f24227f40cdb24210a9a105fce1212
Domonion <artemkononov2@gmail.com>
2018-05-23 05:47:46 +0300

    inplementing student and company solidity contracts

--- company.sol
+++ company.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.4.22;
 
 //https://solidity.readthedocs.io/en/v0.4.15/contracts.html - check for visibility and geters
-//https://solidity.readthedocs.io/en/latest/frequently-asked-questions.html#what-is-the-memory-keyword-what-does-it-do - read entire, easecially about memory
+
+//https://medium.com/coinmonks/storage-vs-memory-in-solidity-8251847186fd - easy way for understanding memory
+
+//https://www.bitdegree.org/learn/solidity-importing/ - about imports easy
+
+import "./student.sol";
 
 contract Company {
     struct certificate {
@@ -10,39 +15,39 @@ contract Company {
         mapping (address => bool) isValid;//do adress has this cert
     }
     
-    address companyAdress;//address of company for this contract
-    string info;//company info in any form : name, date of registration etc...
+    address companyOwner;//address of company for this contract
+    string public info;//company info in any form : name, date of registration etc...
     
      
     constructor(string _info) public{// constructor
         info = _info;
-        companyAdress = msg.sender;
+        companyOwner = msg.sender;
     }
             
             
-    function changeCompanyAdress() public{//if company changes thier adress 
-        if(msg.sender == companyAdress)
-            companyAdress = msg.sender;
+    function changeCompanyAdress(address newOwner) public{//if company changes thier adress 
+        if(msg.sender == companyOwner)
+            companyOwner = newOwner;
     }
                 
     mapping (string => certificate) certificates;
     
     function addCertificate(string name, string picture) public {
-        if (msg.sender != companyAdress) {
+        if (msg.sender != companyOwner) {
             return;
         }
-        certificate memory tmp;//????
+        certificate memory tmp;
         tmp.name = name;
         tmp.picture = picture;
         certificates[name] = tmp;
     }
     
     function giveCertificate(string name, address receiver) public  {
-        if (msg.sender != companyAdress) {
+        if (msg.sender != companyOwner) {
             return;
         }
+        Student dc = Student(receiver);
+        dc.recieveCertificate(name);
         certificates[name].isValid[receiver] = true;
-        address dc = receiver;//???
-        require(dc.call(bytes4(keccak256("recieveCert(string)")), name));
     }
 }
--- student.sol
+++ student.sol
@@ -1,5 +1,51 @@
 pragma solidity ^0.4.22;
 
-contract student{
+import "./company.sol";
+
+contract Student{
+    
+    struct pair{
+        address companyAddress;
+        string cerificateName;
+    }
+    
+    struct vector{
+        uint size;
+        mapping (uint => pair) arr;
+    }
+    
+    vector certificates;
+    
+    address studentOwner;
+    
+    constructor() public{
+        studentOwner = msg.sender;
+    }
+    
+    mapping (address => bool) allowance;//we need this to verify if the recieveCertificate caller can assign us a new certificate
+    
+    function allow(address company) public{
+        if(msg.sender != studentOwner)
+            return;
+        allowance[company] = true;
+    }
+
+    function prohibit(address company) public{
+        if(msg.sender != studentOwner)
+            return;
+        allowance[company] = false;
+    }
+    
+    
+    function recieveCertificate(string cerificateName) public{
+        if(allowance[msg.sender] == false)
+            return;
+        pair memory tmp;
+        tmp.companyAddress = msg.sender;
+        tmp.cerificateName = cerificateName;
+        certificates.arr[certificates.size] = tmp;
+        certificates.size += 1;
+        allowance[msg.sender] = false;
+    }
     
 }
