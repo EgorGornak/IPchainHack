6da887037249b2d8a910092ceae0ea8e1253d13a
Domonion <artemkononov2@gmail.com>
2018-05-23 04:09:25 +0300

    rewriting company interface

--- company.sol
+++ company.sol
@@ -1,31 +1,48 @@
 pragma solidity ^0.4.22;
 
-contract company {
+//https://solidity.readthedocs.io/en/v0.4.15/contracts.html - check for visibility and geters
+//https://solidity.readthedocs.io/en/latest/frequently-asked-questions.html#what-is-the-memory-keyword-what-does-it-do - read entire, easecially about memory
+
+contract Company {
     struct certificate {
-        string name;
-        string picture;
-        mapping (address => bool) isValid;
+        string name;//name of cert
+        string picture;//cert
+        mapping (address => bool) isValid;//do adress has this cert
+    }
+    
+    address companyAdress;//address of company for this contract
+    string info;//company info in any form : name, date of registration etc...
+    
+     
+    constructor(string _info) public{// constructor
+        info = _info;
+        companyAdress = msg.sender;
+    }
+            
+            
+    function changeCompanyAdress() public{//if company changes thier adress 
+        if(msg.sender == companyAdress)
+            companyAdress = msg.sender;
     }
                 
-    address Company = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9; // for example
-    mapping (string => certificate) Certificates;
+    mapping (string => certificate) certificates;
     
-    function addCertificate(string pic, string name) public  {
-        if (msg.sender != Company) {
+    function addCertificate(string name, string picture) public {
+        if (msg.sender != companyAdress) {
             return;
         }
-        certificate tmp;
+        certificate memory tmp;//????
         tmp.name = name;
-        tmp.picture = pic;
-        Certificates[name] = tmp;
+        tmp.picture = picture;
+        certificates[name] = tmp;
     }
     
     function giveCertificate(string name, address receiver) public  {
-        if (msg.sender != Company) {
+        if (msg.sender != companyAdress) {
             return;
         }
-        Certificates[name].isValid[receiver] = true;
-        address dc = receiver;
+        certificates[name].isValid[receiver] = true;
+        address dc = receiver;//???
         require(dc.call(bytes4(keccak256("recieveCert(string)")), name));
     }
 }
